975fd15d595a497405731a468120a6401e5f94be
Zachary Coates <zacharycoates@MacBook-Pro.local>
2024-10-01 18:32:38 -0400

    Added React components for profile_mistake, firstcomp, bios, gallery_props, and props_item

--- src/pages/qcomps/firstcomp.tsx
+++ src/pages/qcomps/firstcomp.tsx
@@ -1,1 +1,14 @@
-function MyComp() {}
+import React from 'react';
+
+export default function FirstComp() {
+  return (
+    <div>
+      <h1>My first component</h1>
+      <ol>
+        <li>Components: UI Building Block</li>
+        <li>Defining a Component</li>
+        <li>Using a component</li>
+      </ol>
+    </div>
+  );
+}
--- src/pages/qcomps/todos.tsx
+++ src/pages/qcomps/todos.tsx
@@ -12,10 +12,11 @@ const person = {
 export default function TodoList() {
   return (
     <div style={person.theme}>
-      <h1>Person Name's Todos</h1>
-      <img className="avatar"
-      src="baseURL + imageID + imageSize + .jpg"
-      alt="Person's Name"
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={`${baseUrl}${person.imageId}${person.imageSize}.jpg`}
+        alt={person.name}
       />
     </div>
   );
